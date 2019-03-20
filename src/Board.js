defadeed48bcff2bee10e9b8df593c75714ce3b2
Stéphane Dryefus <floatingrock@gmail.com>
2019-03-19 17:05:52 -0700

    Add functionality: boxes appear, color changes. Need css.

--- src/Board.js
+++ src/Board.js
@@ -8,40 +8,46 @@ import colors from './colors.js';
 
 class Board extends Component {
   static defaultProps = {
-    numBoxes: 16
-    // colors eventually
+    numBoxes: 16,
+    colors: colors
   };
 
   constructor(props) {
     super(props);
-    this.state = { boxes: Array.from({ length: props.numBoxes }) };
+    this.state = { 
+        boxes:  Array.from({ length: props.numBoxes })
+                .map(n => this.randColor()) };
+
     this.handleClick = this.handleClick.bind(this);
   }
 
   /** generate a random number */
-  randNum() {
-    return Math.floor(Math.random());
+  randNum(range) {
+    return Math.floor(Math.random() * range);
   }
   
+  // Chooses a random color from colors array.
   randColor() {
-   let idx = this.randNum() * this.colors.length;
+   let idx = this.randNum(this.props.colors.length);
    return colors[idx];
   }
 
-  boardGen() {
-    this.setState(curState => ({
-      nums: curState.boxes.map(
-          n => this.randColor())
-    }));
-  }
 
+// Currently unused.
+//   boardGen() {
+//     this.setState(curState => ({
+//       boxes: curState.boxes.map(n => this.randColor())
+//     }));
+//   }
+
+  // Choose a random box whose color to change.
   randBox() {
-    let idx = this.randNum() * this.boxes.length;
+    let idx = this.randNum(this.state.boxes.length);
     let copy = [...this.state.boxes];
     copy[idx] = this.randColor();
     this.setState({boxes: copy});}
 
-
+// Currenlty unusewd
 //   this.setState(st => ({
 //     boxes: st.boxes.map(boxes => ({
 //         boxes[idx] === [idx]
